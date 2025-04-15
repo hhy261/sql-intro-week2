b2916bd814b11ea687b530086ab42c33e3c6c909
Amelie Hwang <hayoung.hwang@kellogg.northwestern.edu>
2025-04-15 06:49:20 +0000

    homework

--- lab-4-3.sql
+++ lab-4-3.sql
@@ -1,5 +1,14 @@
 -- Who hit the most home runs in 2019, and what team did they play for?
 
+Select teams.name, players.first_name, players.last_name, stats.home_runs
+FROm stats
+inner join teams on teams.id = stats.team_id
+inner join players on players.id = stats.player_id
+where teams.year = 2019
+ORDER BY stats.home_runs DESC
+LIMIT 1;
+
+
 -- Expected result:
 --
 -- +---------------+------------+-----------+-----------+
